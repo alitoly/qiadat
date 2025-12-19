5d699058df2c8010f7fe290080a129399ab6c64a
alitoly <alitoly24@gmail.com>
2025-12-19 13:22:28 +0400

    add logo in ligin page

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -88,7 +88,7 @@ export function LoginForm({
                     </form>
                     <div className="bg-muted relative hidden md:block">
                         <img
-                            src="/images/logo-gold.jpg"
+                            src="/images/logo-Gold.jpg"
                             alt="Image"
                             className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
                         />
